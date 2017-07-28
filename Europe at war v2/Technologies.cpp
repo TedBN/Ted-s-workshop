c17247b457622b6c693f5bba48036bf55001b1c6
TedBN <atanaso5@uni.coventry.ac.uk>
2017-07-28 14:06:40 +0300

    Addition of default constructor and docstring update
    Added a definition for the declared default constructor.
    Added a piece of information to docstring

--- Europe at war v2/Technologies.cpp
+++ Europe at war v2/Technologies.cpp
@@ -10,12 +10,23 @@ Technology::Technology (int id_a, std::string name_a, int cost, int level_a)
       *  and must reached the given number to research a technology. Technologies may
       *  be repeatedly researched (Hearts of Iron style) - such technologies must have
       *  a level of 1 to 10. One time technologies need to have level 0. For repeated
-      *  technologies level increases science costs.                                     */
+      *  technologies level increases science costs. Effects will be handled by controller
+      *  class.                                                                                 */
 
     id = id_a;
     name = name_a;
     science_cost = cost;
     level = level_a;
+}
+
+Technology::Technology()
+{
+    /**  To be used by other classes for initializing technology objects.       */
+
+    id = 100;
+    name = "None";
+    science_cost = 0;
+    level = 0;
 }
 
 void Technology::add_prerequisite_tech(Technology tech, int level_a)
